9527beff25f47ecbae3fdca5e14caf4401c75afc
Viviancat666 <vivian9069@gmail.com>
2024-02-08 01:22:18 +0200

    Create articles.js

---
+++ maistori/articles.js
@@ -0,0 +1,17 @@
+const articles = [
+    {
+        articleId: 1,
+        articleTitle: 'Обзавеждане на детска стая',
+        imageUrl: 'https://i.pinimg.com/originals/29/99/e6/2999e621ab047ca74b158f3ea83d9ae6.jpg',
+        content: ''
+
+    },
+    {
+        articleId: 2,
+        articleTitle: 'Как да лепим тапети като професионалисти',
+        imageUrl: '',
+        content: ''
+
+
+    }
+]
